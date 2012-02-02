07f06b7ef1044cd8b20f817f83ae659fd1388961
Andrew Heald <andrew@heald.co.uk>
2012-02-02 14:34:26 +0000

    Try reading from the serial port.

--- script/sample.rb
+++ script/sample.rb
@@ -2,16 +2,14 @@ require 'rubygems'
 require 'serialport'
 
 SerialPort.open(ARGV[0], ARGV[1].to_i, 8, 1) do |port|
-  port.read_timeout = 1000
+  port.read_timeout = 100
   port.flow_control = SerialPort::NONE
   buffer = ''
   loop do
     begin
       buffer += port.read
-      if buffer =~ /(<msg>(.+?)<\/msg>)/im
-        if $1 =~ /<src>(.+?)<\/src>.*?<time>(\d\d\:\d\d\:\d\d)<\/time>.*?<tmpr>0{0,1}(\d{1,2}\.\d)<\/tmpr>.*?<watts>0{0,4}(\d{1,5})<\/watts>/i
-          puts "reading = #{$1} #{$2} #{$3} #{$4}"
-        end
+      if buffer =~ /(<msg><src>(.+?)<\/src>.*?<time>(\d\d\:\d\d\:\d\d)<\/time><tmpr>0{0,1}(\d{1,2}\.\d)<\/tmpr>.*?<watts>0{0,4}(\d{1,5})<\/watts>.*?<\/msg>)/im
+        puts "reading = #{$1} #{$2} #{$3} #{$4}"
         buffer = ''
       end
     rescue
