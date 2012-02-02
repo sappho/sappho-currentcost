439052ed874f345ae860ff41d39b9a58182711e1
Andrew Heald <andrew@heald.co.uk>
2012-02-02 14:13:58 +0000

    Try reading from the serial port.

--- script/sample.rb
+++ script/sample.rb
@@ -9,8 +9,8 @@ SerialPort.open(ARGV[0], ARGV[1].to_i, 8, 1) do |port|
     begin
       buffer += port.read
       if buffer =~ /(<msg>(.+?)<\/msg>)/im
-        if $1 =~ /<src>(.+?)<\/src>.*?<tmpr>0{0,1}(\d+\.\d+)<\/tmpr>.*?<watts>0{0,4}(\d+)<\/watts>/i
-          puts "reading = #{$1} #{$2} #{$3}"
+        if $1 =~ /<src>(.+?)<\/src>.*?<tmpr>0{0,1}(\d{1,2}\.\d)<\/tmpr>.*?<watts>0{0,4}(\d{1,5})<\/watts>/i
+          puts "reading = #{$1} #{$2.to_d} #{$3.to_i}"
         end
         buffer = ''
       end
