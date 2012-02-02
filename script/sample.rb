86bb4e065fcf1fb6332493285ed9ce882463e4ba
Andrew Heald <andrew@heald.co.uk>
2012-02-02 13:22:11 +0000

    Try reading from the serial port.

--- script/sample.rb
+++ script/sample.rb
@@ -1,13 +1,13 @@
 require 'rubygems'
 require 'serialport'
 
-port = SerialPort.new ARGV[0], ARGV[1].to_i
+port = SerialPort.new ARGV[0]
+port.baud = ARGV[1].to_i
 port.read_timeout = 1000
 
 buffer = ''
 loop do
   begin
-    puts 'wait'
     buffer += port.read(1)
     puts buffer
     if buffer =~ /(<msg>(.+?)<.msg>)/im
