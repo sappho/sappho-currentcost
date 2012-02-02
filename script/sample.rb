45b9e9c6521e8e9a2351f8b98cd41f2c7eda5e50
Andrew Heald <andrew@heald.co.uk>
2012-02-02 17:33:22 +0000

    Reduce timeout.

--- script/sample.rb
+++ script/sample.rb
@@ -11,7 +11,7 @@ url = ARGV[2]
 updatecode = ARGV[3]
 
 SerialPort.open(ARGV[0], ARGV[1].to_i, 8, 1) do |port|
-  port.read_timeout = 100
+  port.read_timeout = 10
   port.flow_control = SerialPort::NONE
   buffer = ''
   loop do
