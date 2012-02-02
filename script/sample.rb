a6e7910ec76a7145bab9ae3de5fb747a909964a5
Andrew Heald <andrew@heald.co.uk>
2012-02-02 15:45:09 +0000

    Try reading from the serial port.

--- script/sample.rb
+++ script/sample.rb
@@ -8,8 +8,10 @@ SerialPort.open(ARGV[0], ARGV[1].to_i, 8, 1) do |port|
   loop do
     begin
       buffer += port.read
-      if buffer =~ /<msg><src>(.+?)<\/src>.*?<time>(\d\d\:\d\d\:\d\d)<\/time><tmpr>0{0,1}(\d{1,2}\.\d)<\/tmpr>.*?<watts>0{0,4}(\d{1,5})<\/watts>.*?<\/msg>/im
-        puts "reading = #{$1} #{$2} #{$3} #{$4}"
+      if buffer =~ /<msg><src>(.+?)<\/src>.*?<time>0{0,1}(\d{1,2})\:0{0,1}(\d{1,2})\:0{0,1}(\d{1,2})<\/time><tmpr>0{0,1}(\d{1,2}\.\d)<\/tmpr>.*?<watts>0{0,4}(\d{1,5})<\/watts>.*?<\/msg>/im
+        timestamp = Time.now
+        devtime = Time.local(timestamp.year, timestamp.month, timestamp.day, $2.to_i, $3.to_i, $4.to_i)
+        puts "reading = #{$1} #{timestamp} #{devtime} #{$5} #{$6}"
         buffer = ''
       end
     rescue
