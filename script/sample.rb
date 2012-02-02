bacaeac812d76e8f1335d53c05d9236e7dfcf8e3
Andrew Heald <andrew@heald.co.uk>
2012-02-02 13:36:20 +0000

    Try reading from the serial port.

--- script/sample.rb
+++ script/sample.rb
@@ -3,7 +3,11 @@ require 'serialport'
 
 port = SerialPort.new ARGV[0]
 port.baud = ARGV[1].to_i
+port.data_bits = 8
+port.stop_bits = 1
+port.parity = SerialPort::NONE
 port.read_timeout = 1000
+port.flow_control = SerialPort::NONE
 
 buffer = ''
 loop do
