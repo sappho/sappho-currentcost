e57e55bda8ccf8ad8bb07af1557cd14e460f86ca
Andrew Heald <andrew@heald.co.uk>
2012-02-03 10:46:47 +0000

    Finalise logged content.

--- script/sample.rb
+++ script/sample.rb
@@ -11,21 +11,21 @@ url = ARGV[2]
 updatecode = ARGV[3]
 
 SerialPort.open(ARGV[0], ARGV[1].to_i, 8, 1) do |port|
-  port.read_timeout = 10
+  port.read_timeout = 100
   port.flow_control = SerialPort::NONE
   buffer = ''
   loop do
     buffer += port.read
-    if buffer =~ /<msg><src>(.+?)<\/src>.*?<time>0{0,1}(\d{1,2})\:0{0,1}(\d{1,2})\:0{0,1}(\d{1,2})<\/time><tmpr>0{0,1}(\d{1,2}\.\d)<\/tmpr>.*?<watts>0{0,4}(\d{1,5})<\/watts>.*?<\/msg>/im
+    if buffer =~ /<msg>.*?<tmpr>0{0,1}(\d{1,2}\.\d)<\/tmpr>.*?<watts>0{0,4}(\d{1,5})<\/watts>.*?<\/msg>/im
       timestamp = Time.now
       buffer = ''
-      logger.info "reading: #{$1}  #{timestamp}  temp(c) #{$5}  power(w) #{$6}"
+      logger.info "reading: #{timestamp}  temp(c) #{$1}  power(w) #{$2}"
       begin
         RestClient.post "#{url}/sample/create",
             'updatecode' => updatecode,
             'timestamp' => timestamp,
-            'temperature' => Float($5),
-            'power' => $6.to_i
+            'temperature' => Float($1),
+            'power' => $2.to_i
       rescue Exception => ex
         logger.error ex
       end
