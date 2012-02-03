9ccf4ef1132fb47f8c04e39a483209c7e3139c80
Andrew Heald <andrew@heald.co.uk>
2012-02-03 17:07:11 +0000

    Try to fix sample record create, to no avail - more later.

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -5,13 +5,15 @@ class SamplesController < ApplicationController
     sample = params[:sample]
     devices = Device.where :upload_code => sample[:upload_code]
     if devices.size == 1
-      sample[:device_id] = devices[0].id
+      sample[:device] = devices[0]
       @sample = Sample.new(sample)
       if @sample.save
         format.json { render :json => @sample, :status => :created, :location => @sample }
       else
         format.json { render :json => @sample.errors, :status => :unprocessable_entity }
       end
+    else
+      format.json { render :json => {}, :status => :unauthorized }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ SapphoCurrentCost::Application.routes.draw do
 
   resources :devices
 
-  post "samples/create"
+  resources :samples
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
